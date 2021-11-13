5fbefa1f5601c0baa4d8de54fe1e2bfac79d41fc
Zak Liles <zak.liles@gmail.com>
2021-11-13 15:50:00 -0700

    Completed Problem 10

--- practice.js
+++ practice.js
@@ -87,7 +87,15 @@ let devMountainClassPet = dog['name'];
 // with NAMEPARAM being the name parameter being passed in (not literally NAMEPARAM)
 
 // Code here
-
+function nameCheck(name){
+  if (name === 'Steven'){
+    return 'What is up Steven?';
+  } else if (name === 'Bryan'){
+    return 'Hey Bryan!';
+  } else {
+    return `Cool name, #{name}`;
+  }
+}
 //////////////////PROBLEM 11////////////////////
 
 // Create a function called add that takes in two parameters
